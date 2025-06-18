55245129baffa4e1f29f91ad971804f3c0573f7d
madoka0204 <24261078@kyoto-wu.ac.jp>
2025-06-18 11:20:41 +0900

    Add files via upload

---
+++ urlqr.py
@@ -0,0 +1,26 @@
+import qrcode
+img=qrcode.make("http://wwww.kyoto-wu.ac.jp")
+img.save("kyotowu.png")
+
+with open("kyotowu.png","wb")as f:
+    img.save(f,format="PNG")
+
+
+import streamlit as st
+import qrcode
+import io
+st.title("URLをQRCodeに変換")
+
+with st.form(key="url-input",clear_on_submit=True):
+    url=st.text_input("URL:")
+    button=st.form_submit_button("変換")
+if url:
+    img=qrcode.make(url)
+
+    with io.BytesI0() as f:
+        img.save(f,format="PNG")
+        png=f.getvalue()
+    st.write(url)
+    st.image(png)
+    st.download_button("Download",data=png,
+                       file_name="urlqr.png")
